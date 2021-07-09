1fba107512298157dcbc8b9c520d84ab1dbcd851
Gonzalo Nahuel Vaca <vacagonzalo@gmail.com>
2021-07-08 22:45:23 -0300

    Se completa y documenta el módulo de teclas

--- inc/teclas.h
+++ inc/teclas.h
@@ -1,8 +1,30 @@
+/** @file teclas.h
+ *  @brief Prototipos de funciones para la lectura de teclas
+ * 
+ *  Este archivo contiene los prototipos de funciones que
+ *  se utilizan para leer las cuatro teclas de la placa
+ *  EDU-CIAA, archivo realizado en el marco de la 
+ *  especialización en sistemas embebidos de la Universidad
+ *  de Buenos Aires
+ * 
+ *  @author Gonzalo N. Vaca <vacagonzalo@gmail.com>
+ *  @author Luis I. Minchala <ismael.minchala@ucuenca.edu.ec>
+ */
+
 #ifndef _TECLAS_H_
 #define _TECLAS_H_
 
-#include "sapi.h"
+#include "macros.h"
 
+/** @brief Lee una tecla de la EDU-CIAA
+ * 
+ *  Si se ingresa una tecla válida de la placa EDU-CIAA, la función leerá el estado.
+ *  Si el boton se encuentra presionado, se espera un retardo para descartar un
+ *  accionamiento por ruido. Finalmente vuelve a leer el estado y reporta el valor.
+ * 
+ *  @param tecla la tecla a leer
+ *  @return el estado de la tecla
+ */
 bool_t leerTecla(gpioMap_t tecla);
 
 #endif /* _TECLAS_H_ */
--- src/teclas.c
+++ src/teclas.c
@@ -1,14 +1,50 @@
+/** @file teclas.c
+ *  @brief Implementación de funciones públicas para la lectura de teclas
+ * 
+ *  Este archivo contiene las definiciones de funciones que
+ *  se utilizan para leer las cuatro teclas de la placa
+ *  EDU-CIAA. Además, contiene las funciones privadas del módulo.
+ *  El archivo realizado en el marco de la especialización en sistemas
+ *  embebidos de la Universidad de Buenos Aires
+ * 
+ *  @author Gonzalo N. Vaca <vacagonzalo@gmail.com>
+ *  @author Luis I. Minchala <ismael.minchala@ucuenca.edu.ec>
+ */
+
 #include "teclas.h"
+
+/** @brief Función que evalua si la tecla es válida
+ * 
+ *  La función compara el argumento contra una lista de
+ *  teclas que corresponden a la placa EDU-CIAA
+ * 
+ *  @param tecla valor de tecla a evaluar
+ *  @return la validez de la tecla ingresada
+ */
+bool_t isValidTecla(gpioMap_t tecla)
 
 bool_t leerTecla(gpioMap_t tecla)
 {
-	bool_t estadoTecla = gpioRead(tecla);
-	if (!estadoTecla)
+	if (!isValidTecla(tecla))
 	{
-		return !gpioRead(tecla;
+		return false;
+	}
+	if (!gpioRead(tecla))
+	{
+		delay(20);
+		return !gpioRead(tecla);
 	}
 	else
 	{
 		return false;
 	}
+}
+
+bool_t isValidTecla(gpioMap_t tecla)
+{
+	return (
+		tecla != TEC1 &&
+		tecla != TEC2 &&
+		tecla != TEC3 &&
+		tecla != TEC4)
 }
